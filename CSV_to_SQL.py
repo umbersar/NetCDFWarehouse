826067d815fb6c1584491b2ce77a59a46c817da8
NEXUS\sor035 <SOR035@CSIRO.AU>
2021-11-30 10:38:17 +1100

    [V5] re-ingesting all tables as a clustered columnstore

--- CSV_to_SQL.py
+++ CSV_to_SQL.py
@@ -38,72 +38,74 @@ for FileName in os.listdir(InputFolder):
 # Iterate through list of files and push them into SQL server one at a time 
 FolderStartTime = dt.now()
 print(FolderStartTime, " LOADING STARTED ---------------------------")
-# for FileName in CSVlist:
+for FileName in CSVlist:
     
-#     # Derive relevant Schema and TableNames
-#     Year = FileName[-8:-4]
-#     VariableName = (FileName.split("_")[0])
-#     SchemaName = VariableName.upper()
-#     TableName = "Y"+Year
-#     TableName_Full = "[{}].[{}]".format(SchemaName, TableName)     
-#     ViewName = SchemaName+"_"+TableName
-#     FilePath = InputFolder+"\\"+FileName
+    # Derive relevant Schema and TableNames
+    Year = FileName[-8:-4]
+    VariableName = (FileName.split("_")[0])
+    SchemaName = VariableName.upper()
+    TableName = "Y"+Year
+    TableName_Full = "[{}].[{}]".format(SchemaName, TableName)     
+    ViewName = SchemaName+"_"+TableName
+    IndexName = "COLUMNSTORE_"+ViewName
+    FilePath = InputFolder+"\\"+FileName
 
-#     # Check if the table already exists
-#     cursor.execute("\
-#         SELECT * FROM [information_schema].[tables] \
-#         WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = '{}'".format(SchemaName, TableName))
-#     TableExists = cursor.fetchone()
+    # Check if the table already exists
+    cursor.execute("\
+        SELECT * FROM [information_schema].[tables] \
+        WHERE TABLE_SCHEMA = '{}' AND TABLE_NAME = '{}'".format(SchemaName, TableName))
+    TableExists = cursor.fetchone()
     
-#     # Skip file if the corresponding table name already exists in SQL server
-#     if TableExists:
-#         Print_Duration(FolderStartTime, ("FILE "+ FileName + " SKIPPED: a table with name " +TableName_Full + " already exists in SQL server\n"))
+    # Skip file if the corresponding table name already exists in SQL server
+    if TableExists:
+        Print_Duration(FolderStartTime, ("FILE "+ FileName + " SKIPPED: a table with name " +TableName_Full + " already exists in SQL server\n"))
         
 
-#     else:
-#         Print_Duration(FolderStartTime, ("LOADING " + FileName + " INTO SQL SERVER"))
-#         T1 = dt.now()
-#         print("\t STARTED: ", T1.strftime("%H:%M:%S %p"))
+    else:
+        Print_Duration(FolderStartTime, ("LOADING " + FileName + " INTO SQL SERVER"))
+        T1 = dt.now()
+        print("\t STARTED: ", T1.strftime("%H:%M:%S %p"))
 
-#         cursor.execute("\
-#             IF NOT EXISTS ( \
-#             SELECT  * FROM    [information_schema].[schemata] \
-#             WHERE [SCHEMA_NAME] = '{}' ) \
-#             EXEC('CREATE SCHEMA [{}]');"\
-#                 .format(SchemaName, SchemaName))
+        cursor.execute("\
+            IF NOT EXISTS ( \
+            SELECT  * FROM    [information_schema].[schemata] \
+            WHERE [SCHEMA_NAME] = '{}' ) \
+            EXEC('CREATE SCHEMA [{}]');"\
+                .format(SchemaName, SchemaName))
         
-#         cursor.execute("DROP VIEW IF EXISTS {};".format(ViewName))
-#         CONNECT.commit()
+        cursor.execute("DROP VIEW IF EXISTS {};".format(ViewName))
+        CONNECT.commit()
         
-#         # Create the empty table
-#         cursor.execute("CREATE TABLE {} (\
-#                         [DataID] INT IDENTITY(1,1), \
-#                         [Variable] FLOAT \
-#                         );".format(TableName_Full))
-#         CONNECT.commit()
+        # Create the empty table
+        cursor.execute("CREATE TABLE {} (\
+                        [DataID] INT IDENTITY(1,1), \
+                        [Variable] FLOAT, \
+                        INDEX {} CLUSTERED COLUMNSTORE \
+                        );".format(TableName_Full, IndexName))
+        CONNECT.commit()
         
-#         # Create the empty table view
-#         cursor.execute("CREATE VIEW {} (\
-#                         [Variable]) AS \
-#                             (SELECT [Variable] FROM {});".format(ViewName, TableName_Full))
-#         CONNECT.commit()
-#         T2 = dt.now()
+        # Create the empty table view
+        cursor.execute("CREATE VIEW {} (\
+                        [Variable]) AS \
+                            (SELECT [Variable] FROM {});".format(ViewName, TableName_Full))
+        CONNECT.commit()
+        T2 = dt.now()
         
-#         # Bulk insert the CSV data into the empty view
-#         cursor.execute("BULK INSERT {} \
-#                         FROM '{}' \
-#                         WITH( \
-#                             FIRSTROW = 2, \
-#                             FIELDTERMINATOR = ',', \
-#                             ROWTERMINATOR = '\n', \
-#                             KEEPNULLS \
-#                             )".format(ViewName, FilePath))
-#         CONNECT.commit()
+        # Bulk insert the CSV data into the empty view
+        cursor.execute("BULK INSERT {} \
+                        FROM '{}' \
+                        WITH( \
+                            FIRSTROW = 2, \
+                            FIELDTERMINATOR = ',', \
+                            ROWTERMINATOR = '\n', \
+                            KEEPNULLS \
+                            )".format(ViewName, FilePath))
+        CONNECT.commit()
         
-#         # Delete the view, as changed are automatically in the table now
-#         cursor.execute("DROP VIEW IF EXISTS {};".format(ViewName))
-#         CONNECT.commit()
-#         Print_Duration(T1, "Table Loaded\n")
+        # Delete the view, as changed are automatically in the table now
+        cursor.execute("DROP VIEW IF EXISTS {};".format(ViewName))
+        CONNECT.commit()
+        Print_Duration(T1, "Table Loaded\n")
 
 
 
@@ -118,6 +120,7 @@ for FileName in CSVlist:
     TableName = (FileName.split(".")[0]).split("_")[1]
     TableName_Full = "[{}].[{}]".format(SchemaName, TableName)       
     ViewName = SchemaName+"_"+TableName
+    IndexName = "COLUMNSTORE_"+ViewName
     FilePath = InputFolder+"\\"+FileName
     
 
@@ -147,11 +150,12 @@ for FileName in CSVlist:
         # Specify table formats for either spatial or temporal dimensions
         if "SPACE" in TableName.upper():
             cursor.execute("CREATE TABLE {} (\
-                            [DataID] INT IDENTITY(1,1), \
-                            [SpaceID] INT, \
-                            [Longitude] DECIMAL(5,2) ,\
-                            [Latitude] DECIMAL(5,2) ,\
-                            );".format(TableName_Full))
+                                [DataID] INT IDENTITY(1,1), \
+                                [SpaceID] INT, \
+                                [Longitude] DECIMAL(5,2) ,\
+                                [Latitude] DECIMAL(5,2) ,\
+                                INDEX {} CLUSTERED COLUMNSTORE \
+                                );".format(TableName_Full, IndexName))
             CONNECT.commit()
             
             cursor.execute("CREATE VIEW {} (\
@@ -160,14 +164,15 @@ for FileName in CSVlist:
                             
         elif "TIME" in TableName.upper():
             cursor.execute("CREATE TABLE {} (\
-            [DataID] INT IDENTITY(1,1), \
-            [Year] SMALLINT, \
-            [Month] TINYINT, \
-            [Day] TINYINT, \
-            [Hour] TINYINT, \
-            [Minute] TINYINT, \
-            [Second] TINYINT, \
-            );".format(TableName_Full))
+                                [DataID] INT IDENTITY(1,1), \
+                                [Year] SMALLINT, \
+                                [Month] TINYINT, \
+                                [Day] TINYINT, \
+                                [Hour] TINYINT, \
+                                [Minute] TINYINT, \
+                                [Second] TINYINT, \
+                                INDEX {} CLUSTERED COLUMNSTORE \
+                                );".format(TableName_Full, IndexName))
             CONNECT.commit()
     
             cursor.execute("CREATE VIEW {} (\
