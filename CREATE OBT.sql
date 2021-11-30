11c18d1b9f796088363202cda0d3f393b9a1b5a4
NEXUS\sor035 <SOR035@CSIRO.AU>
2021-11-30 17:39:38 +1100

    Testing out different TSQL syntax for OBT assembly

--- CREATE OBT.sql
+++ CREATE OBT.sql
@@ -1,8 +1,8 @@
 --SELECT DATEADD(
 --SELECT * FROM [CLIMATE_DATA_V1].[DIMENSION].[TIME]
 
---USE [CLIMATE_DATA_V1]
---GO
+USE [CLIMATE_DATA_V1]
+GO
 
 --DROP TABLE IF EXISTS [DIMENSION].[TIME]
 --DROP VIEW IF EXISTS DIM_TIME
@@ -29,31 +29,46 @@
 --                            KEEPNULLS 
 --                            )
 --31
-;WITH [COORDINATE_TABLE] ([DataID], [Year], [Month], [Day], [SpaceID], [Latitude], [Longitude])  AS (
-	SELECT [DS].[DataID], [Year], [Month], [Day], [SpaceID], [Latitude], [Longitude] 
-	FROM 
-		[DIMENSION].[SPACE] AS [DS]
-		JOIN
-		[DIMENSION].[TIME] AS [DT]
-		ON [DS].[DataID] = [DT].[DataID]
-	)
 
-SELECT * FROM [COORDINATE_TABLE]
+--DROP TABLE IF EXISTS [DIMENSION].[ALL]
 
--- AS [DS] JOIN [DIMENSION].[TIME] AS [DT]	ON [DS].[DataID] = [DT].[DataID]))
+----DEFINING CTE HAS WORKED 
+--;WITH [COORDINATE_TABLE] ([DataID], [Month], [Day], [SpaceID], [Latitude], [Longitude])  AS (
+--	SELECT [DS].[DataID], [Month], [Day], [SpaceID], [Latitude], [Longitude] 
+--	FROM 
+--		[DIMENSION].[SPACE] AS [DS]
+--		JOIN
+--		[DIMENSION].[TIME] AS [DT]
+--		ON [DS].[DataID] = [DT].[DataID]
+--	)
 
+----SELECT * FROM [COORDINATE_TABLE]
+
+----UPDATE [COORDINATE_TABLE] SET [Year] = 1911;
+
+----DEFINING WITH SELECT INTO
 --SELECT * INTO [DIMENSION].[ALL]
---FROM 
-	--SELECT [DataID], [Year], [Month], [Day] INTO [DIMENSION].[ALL]
-	--FROM [DIMENSION].[TIME]
-
-	--(SELECT [DS].[DataID], [Year], [Month], [Day], [SpaceID], [Latitude], [Longitude] 
-	--FROM 
-	--	[DIMENSION].[SPACE] AS [DS]
-	--	JOIN
-	--	[DIMENSION].[TIME] AS [DT]
-	--	ON [DS].[DataID] = [DT].[DataID])
+--FROM [COORDINATE_TABLE];
 	
+----UPDATE [DIMENSION].[ALL] SET [Year] = 1911;
+--ALTER TABLE [DIMENSION].[ALL] ADD [Year] SMALLINT DEFAULT 1911 NOT NULL;
+--SELECT TOP(100) * FROM [DIMENSION].[ALL]
+
+--SELECT TOP(100) * FROM (
+--	[DIMENSION].[ALL] AS [DIM]
+--	JOIN
+--	[E0].[Y1911] 
+--	ON [DIM].[DataID] = [E0].[Y1911].[DataID])
+--	ORDER BY [E0].[Y1911].[Variable] DESC
 
 
---SELECT * FROM [COORDINATE_TABLE]
+--SELECT * FROM [E0].[Y1911]
+--UNION ALL
+--SELECT * FROM [E0].[Y1912]
+
+ALTER TABLE [E0].[Y1911] ADD [Year] SMALLINT DEFAULT 1911 NOT NULL
+ALTER TABLE [E0].[Y1912] ADD [Year] SMALLINT DEFAULT 1912 NOT NULL
+
+SELECT * FROM [E0].[Y1911]
+UNION ALL
+SELECT * FROM [E0].[Y1912]
